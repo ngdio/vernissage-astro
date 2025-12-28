f035c717663af4e567866de94001d18feef64247
Niklas Graeber <dev@n1klas.net>
2025-12-28 18:12:11 +0100

    Add demo page site URL to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ngdio.github.io",
+  base: "/vernissage-astro",
+
   image: {
     domains: ["images.unsplash.com"],
   },
