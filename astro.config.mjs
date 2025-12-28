2a1d43d311b0865d9147f3378abc1c15b593812d
Niklas Graeber <dev@n1klas.net>
2025-12-28 18:38:21 +0100

    Use empty string instead of undefined value for default base url (fixes links in header menu)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ngdio.github.io",
-  base: process.env.GITHUB_PAGES === "true" ? "/vernissage-astro" : undefined,
+  base: process.env.GITHUB_PAGES === "true" ? "/vernissage-astro" : "",
 
   image: {
     domains: ["images.unsplash.com"],
