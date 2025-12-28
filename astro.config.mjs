0bd7c6853947ec45cdb5daa712d06f822b9325a4
Niklas Graeber <dev@n1klas.net>
2025-12-28 18:27:18 +0100

    Only add base if building for demo page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ngdio.github.io",
-  base: "/vernissage-astro",
+  base: process.env.GITHUB_PAGES === "true" ? "/vernissage-astro" : undefined,
 
   image: {
     domains: ["images.unsplash.com"],
